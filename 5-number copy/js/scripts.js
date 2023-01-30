637a8b522e343b08d89e363a8c843f83a18c4093
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-01-30 20:09:05 -0300

    aula 6

---
+++ 5-number copy/js/scripts.js
@@ -0,0 +1,14 @@
+var numero = 5;
+console.log(numero);
+console.log(typeof numero);
+
+
+
+var TextoComNumero = '5tt';
+console.log(TextoComNumero);
+console.log(typeof TextoComNumero);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
