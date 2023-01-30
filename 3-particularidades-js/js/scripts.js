1cc72a49f7ece8f637185c3f949c51579f95f301
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-01-30 19:52:34 -0300

    aulas 3,4 ,5

---
+++ 3-particularidades-js/js/scripts.js
@@ -0,0 +1,17 @@
+console.log("primeiro");
+
+console.log("segundo");
+
+console.log("mais um condole");
+
+var a =1;
+a='teste';
+
+console.log(a);
+
+//Este é um comentario
+
+
+/*Este 
+é um comentario
+de multiplas linha*/
---
+++ 4-variaveis/js/scripts.js
@@ -0,0 +1,16 @@
+var teste = 1;
+
+teste = 'Matheus';
+
+console.log(teste);
+
+var MeuPrimeiroNome = 'Tiago'; //camelCase
+console.log(MeuPrimeiroNome);
+
+/*Outras formas de declarar variáveis
+
+let =
+
+const =
+
+*/
