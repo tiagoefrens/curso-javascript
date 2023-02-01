4ca47dd943e6e2f53289f4fb61c5a2916b11cbf1
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-02-01 19:07:07 -0300

    11

---
+++ 11-if copy/js/scripts.js
@@ -0,0 +1,11 @@
+var idade = 17;
+
+console.log("Antes do IF");
+
+if(idade > 18){
+    console.log("Pode fazer a carteira de habilitação");
+} else{
+    console.log("Não pode fazer carteira de habilitação")
+}
+
+console.log("Depois do IF")
