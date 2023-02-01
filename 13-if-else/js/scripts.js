2546b60e0977cb249feb47a643ca8494faf51692
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-02-01 19:15:39 -0300

    12

---
+++ 13-if-else/js/scripts.js
@@ -0,0 +1,14 @@
+var idade = 18;
+var possuicarro = 1;
+
+
+
+if(idade >= 18){
+    console.log("Pode fazer a carteira de habilitação");
+} else{
+    console.log("Não pode fazer carteira de habilitação")
+}
+if(possuicarro){
+    console.log("Pode andar de carro!")
+}
+
