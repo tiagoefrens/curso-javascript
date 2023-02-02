68b761c208ca88d05d5e79983e960a3c1acac269
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-02-02 20:19:32 -0300

    or

---
+++ 16-operador-or/js/scripts.js
@@ -0,0 +1,12 @@
+var idade = 16;
+var nome = "João";
+
+if(nome == "João" || idade == 18){
+    console.log("João pode entrar na aula de esgrima")
+} else {
+    console.log("Não pode entrar")
+}
+
+if(nome == "Joã" && 15 > 20 || 10==10){
+    console.log("testando")
+}
