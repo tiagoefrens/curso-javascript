9793da36d1dbe9da0dd7a45ebabb54208147abef
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-02-04 15:54:00 -0300

    op atribuição

---
+++ 19-op-atribuicao/js/scripts.js
@@ -0,0 +1,19 @@
+var x =1;
+var y = 2;
+
+//SOMA
+console.log(x = x + y);
+console.log(x += y);
+
+//SUBTRAÇÃO
+console.log(x -= y);
+
+//MULTIPLICAÇÃO
+console.log(x *= y);
+
+//DIVISÃO
+console.log(x /= y);
+
+//LOOPS
+console.log(x++);
+console.log(x--);
