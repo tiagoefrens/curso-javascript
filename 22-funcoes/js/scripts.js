d938a0d6daae466476c9968774a08721d8c71b04
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-02-05 09:56:52 -0300

    funcoes

---
+++ 22-funcoes/js/scripts.js
@@ -0,0 +1,30 @@
+function primeiraFuncao(){
+
+    console.log("hello world das funções")
+}
+
+primeiraFuncao();
+
+function dizerNome(nome){
+    console.log("O nome é: " + nome);
+}
+
+dizerNome("Matheus");
+dizerNome("Tiago");
+dizerNome("Lucas");
+
+var nomeDoBancoDeDados = "João";
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a,b){
+    var soma = a + b;
+    return soma;
+}
+var somaUm = soma(2,5);
+console.log(somaUm);
+
+
+
+console.log(soma(5, 5));
+
