c968201cbfc58fdf09018ff09aa39d8b62aaf11f
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-12-03 10:15:19 -0300

    eventos e onload

---
+++ 46-eventos-onload/js/scripts.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    console.log("Carregou o DOM");
+
+
+    
+var title2 = document.querySelector("#title");
+console.log(title2);
+}
+
+console.log("Carregou o JS");
+
+var title = document.querySelector("#title");
+console.log(title);
