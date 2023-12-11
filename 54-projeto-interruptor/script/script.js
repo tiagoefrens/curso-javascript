65d91dacb3b3e0932c2e835c0e78055e922ee65c
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-12-11 20:38:03 -0300

    projeto interruptor

---
+++ 54-projeto-interruptor/script/script.js
@@ -0,0 +1,8 @@
+var fundo = document.querySelector("body").addEventListener("click",acender);
+
+function acender(){
+    var body = document.querySelector('body');
+    body.classList.toggle('aceso');
+   
+}
+
