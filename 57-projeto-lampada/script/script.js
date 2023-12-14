649091c69ac1add84daf97233ebda095b0ce0f0a
Tiago Efrens <98785464+tiagoefrens@users.noreply.github.com>
2023-12-14 20:24:58 -0300

    projeto lampada

---
+++ 57-projeto-lampada/script/script.js
@@ -0,0 +1,35 @@
+const  turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+
+function isLampBroken(){
+    return lamp.src.indexOf ('quebrada') > -1
+}
+
+function lampBroken(){
+    lamp.src="./img/quebrada.jpg";
+    
+}
+
+function lampOff(){
+    if( !isLampBroken () ){
+        lamp.src="./img/desligada.jpg";
+    } 
+    
+}
+
+function lampOn(){
+    if( !isLampBroken () ){
+    lamp.src ="./img/ligada.jpg";
+    }
+}
+
+
+
+turnOn.addEventListener('click', lampOn);
+
+turnOff.addEventListener('click', lampOff);
+
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+lamp.addEventListener('dblclick', lampBroken);
